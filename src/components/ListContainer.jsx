85876642ff43978237ea3261d0648c359450322c
LissyMoon <eliza.twil16@gmail.com>
2020-02-25 15:25:14 +0300

    повторные слетевшие правки в контейнере с айтемами

--- src/components/ListContainer.jsx
+++ src/components/ListContainer.jsx
@@ -2,14 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  selected: {
+    background: '#8567ab !important',
+    color: '#ffffff !important',
+  },
+}));
 
 const ListWrapper = styled.div`
   background: white;
-  height: 500px;
+  height: 50%;
   width: 400px;
   padding: 15px 0;
   margin: 0;
   border-radius: 15px;
+`;
+
+const ListItemsWrapper = styled(List)`
+  overflow-y: auto;
+  height: calc(100% - 30px);
+  margin: 8px 0 !important;
+  ::-webkit-scrollbar { width: 1px; height: 3px;}
+  ::-webkit-scrollbar-track {  background-color: #999;}
+  ::-webkit-scrollbar-track-piece { background-color: #ffffff;}
+  ::-webkit-scrollbar-thumb { height: 50px; background-color: #666; border-radius: 3px;}
 `;
 
 const ListItemWrapper = styled(ListItem)`
@@ -18,6 +36,7 @@ const ListItemWrapper = styled(ListItem)`
     font-weight: 400;
     font-size: 1rem;
     padding: 16px !important;
+    word-break: break-all;
     &:hover {
         background: #8567ab !important;
         color: white !important; 
@@ -26,17 +45,19 @@ const ListItemWrapper = styled(ListItem)`
 
 const ListContainer = () => {
     const [selectedIndex, setSelectedIndex] = React.useState(null);
+    const classes = useStyles();
 
     const handleListItemClick = (index) => e => {
         setSelectedIndex(index);
       }
     return (
       <ListWrapper>
-          <List>
+          <ListItemsWrapper>
               <ListItemWrapper
               button
               selected={selectedIndex === 0}
               onClick={handleListItemClick(0)}
+              className={selectedIndex === 0 && classes.selected}
               >
                 Дело 1
               </ListItemWrapper>
@@ -89,8 +110,35 @@ const ListContainer = () => {
               >
                 Дело 8
               </ListItemWrapper>
-          </List>
-        
+              <ListItemWrapper
+              button
+              selected={selectedIndex === 7}
+              onClick={handleListItemClick(7)}
+              >
+                Дело 8
+              </ListItemWrapper>
+              <ListItemWrapper
+              button
+              selected={selectedIndex === 8}
+              onClick={handleListItemClick(8)}
+              >
+                Дело 9
+              </ListItemWrapper>
+              <ListItemWrapper
+              button
+              selected={selectedIndex === 9}
+              onClick={handleListItemClick(9)}
+              >
+                Дело 10
+              </ListItemWrapper>
+              <ListItemWrapper
+              button
+              selected={selectedIndex === 10}
+              onClick={handleListItemClick(10)}
+              >
+                Дело 11
+              </ListItemWrapper>
+          </ListItemsWrapper>
       </ListWrapper>
     );
   }
